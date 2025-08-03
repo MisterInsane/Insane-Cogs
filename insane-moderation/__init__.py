a2f8d05d9974bbea1beb9f4c4adecf54fd809fd4
GayNerd <gaynerd97@gmail.com>
2025-08-03 02:23:08 -0400

    Initial commit

---
+++ insane-moderation/__init__.py
@@ -0,0 +1,9 @@
+from .modslash import ModSlash
+
+async def setup(bot):
+    """
+    The setup function for the ModSlash cog.
+    This is called by Redbot to load the cog.
+    """
+    await bot.add_cog(ModSlash(bot))
+
